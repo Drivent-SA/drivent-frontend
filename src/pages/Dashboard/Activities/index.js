c128166bcc1d32df9f9866bd902faced7df258a7
rodrigocqb <rodrigocortibarros@gmail.com>
2022-12-16 14:19:29 -0300

    feat: add error messages to activities page

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,33 @@
+import { Typography } from '@material-ui/core';
+import { useState } from 'react';
+import ErrorMessageWrapper from '../../../components/ErrorMessageWrapper';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const [ticketData, setTicketData] = useState();
+
+  function showError() {
+    if (ticketData?.status !== 'PAID') {
+      return (
+        <ErrorMessageWrapper>
+          Você precisa ter confirmado pagamento antes de fazer a escolha de atividades
+        </ErrorMessageWrapper>
+      );
+    }
+    if (ticketData?.ticketType?.isRemote) {
+      return (
+        <ErrorMessageWrapper>
+          {`Sua modalidade de ingresso não necessita escolher
+          atividade. Você terá acesso a todas as atividades.`}
+        </ErrorMessageWrapper>
+      );
+    }
+    return <></>;
+  }
+
+  return (
+    <>
+      <Typography variant="h4">Escolha de atividades</Typography>
+      {showError()}
+    </>
+  );
 }
