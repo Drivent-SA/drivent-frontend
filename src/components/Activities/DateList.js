17bfe03e328b4dc37426d45933cd19efa0a3b7db
rodrigocqb <rodrigocortibarros@gmail.com>
2022-12-18 19:27:18 -0300

    feat: add functionality to filter activities by day

---
+++ src/components/Activities/DateList.js
@@ -0,0 +1,44 @@
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import styled from 'styled-components';
+import DateActivities from './DateActivities';
+
+export default function DateList({ activities }) {
+  dayjs.extend(updateLocale);
+  dayjs.updateLocale('pt-br', {
+    weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  });
+
+  const dates = [];
+  activities !== null &&
+    activities.forEach((value) => {
+      if (dates.includes(value.date) === false) {
+        dates.push(value.date);
+      }
+    });
+
+  return (
+    <>
+      <Subtitle>Primeiro, filtre pelo dia do evento:</Subtitle>
+      <ButtonContainer>
+        {dates.map((date, index) => (
+          <DateActivities key={index} date={dayjs(date).locale('pt-br').format('dddd, DD/MM')} />
+        ))}
+      </ButtonContainer>
+    </>
+  );
+}
+
+const Subtitle = styled.h3`
+  margin-top: 34px;
+  margin-bottom: 23px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 20px;
+  color: #8e8e8e;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  column-gap: 17px;
+`;
