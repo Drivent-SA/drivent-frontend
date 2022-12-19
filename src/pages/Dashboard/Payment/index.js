4ed9e4bddb55b65c00cd34ad13060584588ece28
Pedro Pereira <plp.leoncio@gmail.com>
2022-12-19 19:36:23 -0300

    feat: resumo do ingresso escolhido na area de pagamentos

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,20 @@
+import ErrorMessageWrapper from '../../../components/ErrorMessageWrapper';
+import PaymentsComponent from '../../../components/Payments/PaymentsComponent';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const { ticket } = useTicket();
+
+  function showError() {
+    if (ticket?.ticketType?.price !== undefined) {
+      return (<PaymentsComponent/>);
+    } else {
+      return <ErrorMessageWrapper>Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso</ErrorMessageWrapper>;
+    }
+  }
+  return (
+    <>
+      {showError()}
+    </>
+  );
 }
