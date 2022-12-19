a2cec7cb2861b8a84b1b19e535b4b7bd44b4e8f9
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-12-19 15:01:14 -0300

    feat: now the user can see the available hotels

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,37 @@
+import { Typography } from '@material-ui/core';
+import ErrorMessageWrapper from '../../../components/ErrorMessageWrapper';
+import HotelsList from '../../../components/Hotels/HotelsList';
+import useHotels from '../../../hooks/api/useHotels';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const { ticket } = useTicket();
+  const { hotels } = useHotels();
+
+  function showError() {
+    if (ticket?.status !== 'PAID') {
+      return (
+        <ErrorMessageWrapper>
+          Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem
+        </ErrorMessageWrapper>
+      );
+    }
+    if (ticket?.TicketType?.isRemote || !ticket?.TicketType?.includesHotel) {
+      return (
+        <ErrorMessageWrapper>
+          {`Sua modalidade de ingresso não inclui hospedagem
+          Prossiga para a escolha de atividades`}
+        </ErrorMessageWrapper>
+      );
+    }
+    return false;
+  }
+
+  return (
+    <>
+      <Typography variant="h4">Escolha de hotel e quarto</Typography>
+      {showError()}
+      {showError() === false ? <HotelsList hotels={hotels} /> : <></>}
+    </>
+  );
 }
